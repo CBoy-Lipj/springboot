152fa9a51bfd1c70dd91c1797b88903eba635e14
zzy <2696949240@qq.com>
2020-04-14 18:59:01 +0800

    zzy添加了一个Test测试类

---
+++ src/test/java/com/lpj/boot/Test.java
@@ -0,0 +1,21 @@
+package com.lpj.boot;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author 邹靓仔
+ * @date 2020/4/14 -18:55 -springboot02
+ **/
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class Test {
+
+
+    @org.junit.Test
+    public void test01(){
+
+    }
+}
